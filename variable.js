0acd40a4436ae87c8bcab8549485b4bc1dabe6ab
Mark <mrkcarlos576@gmail.com>
2024-03-14 11:30:08 +0300

    intro to js variables

---
+++ variable.js
@@ -0,0 +1,22 @@
+let age = 45
+
+console.log(age)
+
+console.log(age)
+
+age = 34;
+
+console.log(age)
+
+const school = "KU"
+
+console.log(school)
+
+var fullname = "Carlos";
+
+console.log(fullname)
+
+fullname = "Joy";
+
+console.log(fullname);
+
